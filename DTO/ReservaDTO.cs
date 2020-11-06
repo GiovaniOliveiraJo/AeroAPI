663ea87e8f5195ee5b0cb6ca93bdd20ec3a05f7a
GiovaniOliveiraJo <giovani_oliveira125@hotmail.com>
2020-11-05 22:42:11 -0300

    AERO API – ATIVIDADE 3: BUSCA DE VÔOS COM FILTRO

---
+++ DTO/ReservaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AeroAPI.DTO
+{
+    public class ReservaDTO
+    {
+        
+        public int VooId { get; set; }
+        
+        public string Documento { get; set; }
+              
+        public int Poltrona { get; set; }
+    }
+}
